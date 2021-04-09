6475849741a80e1e57dea621ea8be33fbf9628f1
Pau <pvalverdemolina@gmail.com>
2021-04-10 08:31:19 +0900

    Added useState hook to save the input state of the text area

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+  const [wordsInput, setWordsInput] = useState("")
+
+  function handleChange(event) {
+    setWordsInput(event.target.value)
+  }
+
+  console.log(wordsInput)
+
   return (
     <div>
       <h1>Speed Typing Game</h1>
-      <textarea value=""/>
-      <h4>Time remaining: ???
-
-      </h4>
+      <textarea value={wordsInput} onChange={handleChange}/>
+      <h4>Time remaining: ???</h4>
       <button>Start!</button>
       <h1>Word Count: </h1>
     </div>
