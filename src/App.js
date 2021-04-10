d7bc4bfb79080b8d03c6a8976cd2d03609d969e9
Pau <pvalverdemolina@gmail.com>
2021-04-11 08:45:35 +0900

    the start button now triggers the timer.
    when timer runs out words are counted and stored in state, then displayed on screen.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const [text, setText] = useState("")
   const [timer, setTimer] = useState(5)
+  const [isGameRunning, setIsGameRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
 
   function handleChange(event) {
     setText(event.target.value)
@@ -15,23 +17,28 @@ function App() {
     const wordCount = text.trim().split(" ")
     return wordCount.filter(word => word !== "" ).length
   }
+
+  function startGame() {
+    setIsGameRunning(true)
+  }
   
   useEffect(() => {
-    if (timer > 0) {
+    if (timer > 0 && isGameRunning) {
       setTimeout(() => setTimer(prevState => prevState - 1), 1000)
+    } else if (timer === 0){
+      setIsGameRunning(false)
+      setWordCount(countWords(text))
     }
-  }, [timer])
+  }, [timer, isGameRunning, text])
 
   return (
     <div>
       <h1>Speed Typing Game</h1>
       <textarea value={text} onChange={handleChange}/>
       <h4>Time remaining: {timer}</h4>
-      <button onClick={() => console.log(countWords(text))}>Start!</button>
-      <h1>Word Count: </h1>
+      <button onClick={startGame}>Start!</button>
+      <h1>Word Count: {wordCount} </h1>
     </div>
-    
-
   );
 }
 
