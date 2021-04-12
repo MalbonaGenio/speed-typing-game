37f52be70a352819895a2c2c731e59eb21214c31
Pau <pvalverdemolina@gmail.com>
2021-04-12 10:09:52 +0900

    added a disabled style for text area. Added a useRef hook to focus on the textbox after button is clicked.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import './App.css';
 
 function App() {
+  const gameLength = 5
   const [text, setText] = useState("")
-  const [timer, setTimer] = useState(5)
+  const [timer, setTimer] = useState(gameLength)
   const [isGameRunning, setIsGameRunning] = useState(false)
   const [wordCount, setWordCount] = useState(0)
+  const textareaRef = useRef(null)
 
   function handleChange(event) {
     setText(event.target.value)
@@ -19,24 +21,36 @@ function App() {
   }
 
   function startGame() {
+    setText("")
     setIsGameRunning(true)
+    /*forces a sincronoulsy change in the disabled 
+    property. To avoid preventing the focus to apply to 
+    an still disabled text area due to the async 
+    of the setIsGameRunning function. */
+    textareaRef.current.disabled = false
+    textareaRef.current.focus()
+    setTimer(gameLength)
+  }
+
+  function endGame() {
+    setIsGameRunning(false)
+    setWordCount(countWords(text))
   }
   
   useEffect(() => {
     if (timer > 0 && isGameRunning) {
       setTimeout(() => setTimer(prevState => prevState - 1), 1000)
     } else if (timer === 0){
-      setIsGameRunning(false)
-      setWordCount(countWords(text))
+      endGame()
     }
-  }, [timer, isGameRunning, text])
+  }, [timer, isGameRunning])
 
   return (
     <div>
       <h1>Speed Typing Game</h1>
-      <textarea value={text} onChange={handleChange}/>
+      <textarea ref={textareaRef} disabled={!isGameRunning} value={text} onChange={handleChange}/>
       <h4>Time remaining: {timer}</h4>
-      <button onClick={startGame}>Start!</button>
+      <button disabled={isGameRunning} onClick={startGame}>Start!</button>
       <h1>Word Count: {wordCount} </h1>
     </div>
   );
